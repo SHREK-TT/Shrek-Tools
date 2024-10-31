b9f5b221aff29b945be1d4df756a6cb27d1789af
SHREK-TT <diskeggy@gmail.com>
2024-10-31 20:58:44 +0100

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,9 +1,5 @@
 @echo off
 Title Download Modules...
-python --version 3>NUL
-if errorlevel 1 goto errorNoPython
-pip -v>NUL
-if errorlevel 1 goto errorNoPip
 pip install subprocess
 cd utilities\Settings
 python module.py
