90a59d90766868d71b863b154a23cf68870f3dec
SHREK-TT <diskeggy@gmail.com>
2024-10-31 20:59:32 +0100

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,13 +1,1 @@
-@echo off
-Title Download Modules...
-pip install subprocess
-cd utilities\Settings
-python module.py
-cd ..
-cd ..
-move utilities\Start.bat .
-cd utilities\Settings
-cd ..
-cd ..
-cls
-Start.bat
+pip install -r requirements.txt
